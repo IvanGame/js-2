f039f1babdbfa73d08c034c8301946204ad87f2d
IvanGame <ivan5456007@gmail.com>
2022-08-25 16:33:43 +0700

    My first lesson

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const goods = [
 
 const renderGoodsItem = (good, img = "https://loremflickr.com/280/200?lock=1") => {
     return `
-    <div class="goods-item">
+    <div class="goods-list__item">
     <img src = "${img}">
     <h3>${good.title}</h3>
     <p>${good.price}</p>
@@ -16,7 +16,7 @@ const renderGoodsItem = (good, img = "https://loremflickr.com/280/200?lock=1") =
 };
 
 const renderGoodsList = (list) => {
-    document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item)).join('');
+    document.querySelector('.goods-list__row').innerHTML = list.map(item => renderGoodsItem(item)).join('');
 }
 
 renderGoodsList(goods);
