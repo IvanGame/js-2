cabf7171ac667b792b78b46ec7c7a6b6a7732a86
IvanGame <ivan5456007@gmail.com>
2022-09-05 18:24:25 +0700

    -_-

--- js/main.js
+++ js/main.js
@@ -1,3 +1,83 @@
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    render(img = "https://loremflickr.com/280/200?lock=1") {
+        return `
+        <div class="goods-list__item">
+        <img src = "${img}">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+        <button>Купить</button>
+        </div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+
+    getSum() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        })
+        console.log(`Стоимость всех товаров = ${sum}`);
+    }
+
+    render() {
+        let listHTML = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHTML += goodItem.render();
+        });
+        document.querySelector('.goods-list__row').innerHTML = listHTML;
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.getSum();
+
+class Basket {
+    addGood() {
+
+    }
+
+    removeGood() {
+
+    }
+
+    editGood() {
+
+    }
+
+    render() {
+
+    }
+}
+
+class BasketItem {
+    render() {
+
+    }
+}
+
+
+/*
 const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
@@ -20,3 +100,4 @@ const renderGoodsList = (list) => {
 }
 
 renderGoodsList(goods);
+*/
